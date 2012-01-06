926d2a2c71ef4987921d66215535a0f6054a62af
Jason McMains <jmcmains@gmail.com>
2012-01-05 23:34:18 -0500

    Added a Site Layout

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,11 +1,13 @@
 require 'spec_helper'
 
 describe PagesController do
-  
   render_views
   
+  before(:each) do
+  @base_title = "Ruby on Rails Tutorial Sample App"
+  
   describe "GET 'home'" do
-    it "returns http success" do
+    it "should be successful" do
       get 'home'
       response.should be_success
     end
@@ -13,7 +15,7 @@ describe PagesController do
     it "should have the right title" do
       get 'home'
       response.should have_selector("title", 
-        :content => "Ruby on Rails Tutorial Sample App | Home")
+        :content => @base_title + " | Home")
     end
     
     it "should have a non-blank body" do
@@ -22,18 +24,41 @@ describe PagesController do
   end
   
   describe "GET 'contact'" do
-    it "returns http success" do
+    it "should be successful" do
       get 'contact'
       response.should be_success
     end
+    
+    it "should have the right title" do
+      get 'contact'
+      response.should have_selector("title", 
+        :content => @base_title + " | Contact")
+    end
   end
   
-
+  describe "GET 'help'" do
+    it "should be successful" do
+      get 'help'
+      response.should be_success
+    end
+    
+    it "should have the right title" do
+      get 'help'
+      response.should have_selector("title", 
+        :content => @base_title + " | Help")
+    end
+  end
   
   describe "GET 'about'" do
-    it "returns http success" do
+    it "should be successful" do
       get 'about'
       response.should be_success
+    end
+    
+    it "should have the right title" do
+      get 'about'
+      response.should have_selector("title", 
+        :content => @base_title + " | About")
     end
   end
 end
