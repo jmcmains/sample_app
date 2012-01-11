03cfa205b809b2de1f1c4ab197cd4a78af50298f
Jason McMains <jmcmains@gmail.com>
2012-01-11 17:19:48 -0500

    Working user signin

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,8 @@ class SessionsController < ApplicationController
       @title = "Sign in"
       render 'new'
     else
-      # sign the user in and redirect to the user's show page.
+      sign_in user
+      redirect_to user
     end
   end
   
