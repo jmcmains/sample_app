1cac2995482304e6814d03b53091e097318811d7
Jason McMains <jmcmains@gmail.com>
2012-01-17 17:37:43 -0500

    Working follow/unfollow buttons (with Ajax)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
-  before_filter :authenticate, :only => [:index, :edit, :update, :destroy]
-  before_filter :correct_user, :only => [:edit, :update]
-  before_filter :admin_user,   :only => :destroy
+  before_filter :authenticate, :except  => [:show, :new, :create]
+  before_filter :correct_user, :only    => [:edit, :update]
+  before_filter :admin_user,   :only    => :destroy
   
   def index
     @title = "All users"
@@ -48,6 +48,20 @@ class UsersController < ApplicationController
   def destroy
     @user.destroy
     redirect_to users_path, :flash => {:success => "User destroyed."}
+  end
+  
+  def following
+    @title = "Following"
+    @user = User.find(params[:id])
+    @users = @user.following.paginate(:page => params[:page])
+    render 'show_follow'
+  end
+  
+  def followers
+    @title = "Followers"
+    @user = User.find(params[:id])
+    @users = @user.followers.paginate(:page => params[:page])
+    render 'show_follow'
   end
   
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 SampleApp::Application.routes.draw do
 
-  resources :users
-  resources :sessions, :only => [:new, :create, :destroy]
-  resources :microposts, :only => [:create, :destroy]
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+  
+  resources :sessions,      :only => [:new, :create, :destroy]
+  resources :microposts,    :only => [:create, :destroy]
+  resources :relationships, :only => [:create, :destroy]
   
   root                :to => 'pages#home'
   
@@ -12,6 +18,8 @@ SampleApp::Application.routes.draw do
   match '/signup',    :to => 'users#new'
   match '/signin',    :to => 'sessions#new'
   match '/signout',   :to => 'sessions#destroy'
+  
+  
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
